a542976fbd07defb32392f4eb05a6347cdd4c56a
luisafrota20-boop <luisafrota20@gmail.com>
2026-06-25 18:25:40 -0300

    Update message content on button click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("btnInfo").addEventListener("click", function(){
 
 document.getElementById("mensagem").innerHTML =
-"Meu objetivo é atuar na área de Análise de Dados, utilizando tecnologia para transformar informações em conhecimento e apoiar decisões estratégicas.";
+"Meu objetivo é atuar profissionalmente com Análise de Dados, transformando informações em conhecimento estratégico para auxiliar empresas na tomada de decisões.";
 
 });
