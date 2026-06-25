696fd8b0ca112cab4079b4b0016c1979008ef603
luisafrota20-boop <luisafrota20@gmail.com>
2026-06-25 18:06:21 -0300

    Add event listener to display data analysis goal

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btnInfo").addEventListener("click", function(){
+
+document.getElementById("mensagem").innerHTML =
+"Meu objetivo é atuar na área de Análise de Dados, utilizando tecnologia para transformar informações em conhecimento e apoiar decisões estratégicas.";
+
+});
